ddeda1ce29098d01603b36bc9d9f88a3f44f0cf6
xploiter-projects <mali.bscs15seecs@seecs.edu.pk>
2019-09-03 16:10:57 +0500

    minor changes

--- README.md
+++ README.md
@@ -9,48 +9,24 @@ $ cd PDF2JSON
 $ pip install -r requirements.txt
 ```
 
-### Convert PDF to JSON
+### Usage
 ```
-usage: pdf2json.py [-h] -p PDF_FILES_DIR [-d]
+usage: pdf2json.py [-h] -i PDF_FILE [-s] [-d]
 
 optional arguments:
-  -h, --help        show this help message and exit
-  -p PDF_FILES_DIR  enter the path directory containing pdf files
-  -d, --debug       turn on the debug mode
+  -h, --help         show this help message and exit
+  -i PDF_FILE        enter the path to pdf file
+  -s, --save_images  save images of pdf file
+  -d, --debug        turn on the debug mode
 
 ```
-Example: ```python pdf2json.py -p tests```
 
-#### Directory Structure
-    .
-    ├── tests                   # this directory contains pdf files 
-    |   ├── file1.pdf
-    |   ├── file2.pdf
-    |   ├── file3.pdf
-    |   └── ...                 # etc
+### Run for a pdf file
 
-### Visualize
-```
-usage: visualize.py [-h] -p PDF_FILES_DIR
+Example: ```python pdf2json.py -i tests/example1.pdf```
 
-optional arguments:
-  -h, --help        show this help message and exit
-  -p PDF_FILES_DIR  enter the directory path containing pdf and json files
+### Run for a folder containing pdf files
 
-```
-Example: ```python visualize.py -p tests```
+usage: ./run_for_folder.sh PATH/TO/FOLDER [-s] [-d]
 
-#### Directory Structure for Visualize
-    .
-    ├── tests                   # this directory contains pdf files 
-    |   ├── tesseractJSON       # directory contains JSON files generated from above script 
-    |   |   ├── file1.json
-    |   |   ├── file2.json
-    |   |   ...                 # etc
-    |   ├── blocks              # directory contains JSON files that needs to be compared
-    |   |   ├── file1.json
-    |   |   ├── file2.json
-    |   |   ...                 # etc
-    |   ├── file1.pdf
-    |   ├── file2.pdf
-    |   └── ...                 # etc
+Example: ```./run_for_folder.sh tests -s -d```
--- pdf2json.py
+++ pdf2json.py
@@ -7,10 +7,32 @@ import cv2
 import pdf2image
 import pytesseract
 import numpy as np
-from unidecode import unidecode
 from PIL import Image
 
+import yaml
 
+
+def getConfig():
+    configs = None
+    with open("config.yaml", 'r') as stream:
+        try:
+            configs = yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+    return configs
+
+def createDirs(imageDir=None, fileName=None):
+    pdf_file_name = fileName
+    # creating debug directory
+    if not os.path.exists(imageDir):
+        os.mkdir(imageDir)
+    # creating directory for each pdf file
+    imgs_dir = os.path.join(imageDir, pdf_file_name)
+    if not os.path.exists(imgs_dir):
+        os.mkdir(imgs_dir)
+
+    return imgs_dir
+    
 def getDataForJson(df_ocr, page_image, page_num, output_data):
     # grouping blocks of text using ocr data
     block_group = df_ocr.groupby(["block_num"])
@@ -34,7 +56,7 @@ def getDataForJson(df_ocr, page_image, page_num, output_data):
 
                             if args.debug:
                                 # draw block bounding boxes in green color
-                                cv2.rectangle(page_image, (topL["topleft"]["x"], topL["topleft"]["y"]),(bottomR["bottomright"]["x"], bottomR["bottomright"]["y"]), (0, 255, 0), 2)
+                                cv2.rectangle(page_image, (topL["topleft"]["x"], topL["topleft"]["y"]),(bottomR["bottomright"]["x"], bottomR["bottomright"]["y"]), (0, 0, 255), 2)
                     
                     # add top_left block point
                     block_dict.update(topL)
@@ -47,15 +69,14 @@ def getDataForJson(df_ocr, page_image, page_num, output_data):
                         if len(line["text"].str.cat()) > 0:
                             line_dict = {}
                             text = ""
+                            line_topL = {}
+                            line_bottomR = {}
                             for _, line_row in line.iterrows():
                                 # at level = 4, rows contain bounding boxes on line level
                                 if line_row["level"] == 4:
-                                    line_dict["topleft"] = { "x": line_row["left"], "y": line_row["top"], }
-                                    line_dict["bottomright"] = { "x": line_row["left"] + line_row["width"], "y": line_row["top"] + line_row["height"],}
+                                    line_topL = { "x": line_row["left"], "y": line_row["top"], }
+                                    line_bottomR = { "x": line_row["left"] + line_row["width"], "y": line_row["top"] + line_row["height"],}
 
-                                    if args.debug:
-                                        # draw line bounding boxes in red color
-                                        cv2.rectangle( page_image, (line_dict["topleft"]["x"], line_dict["topleft"]["y"]), (line_dict["bottomright"]["x"], line_dict["bottomright"]["y"]), (200, 100, 0), 1)
                                 # select the non-empty words having confidence > 10%
                                 if ( line_row["conf"] > -1
                                     and not str(line_row["text"]).isspace()
@@ -64,7 +85,9 @@ def getDataForJson(df_ocr, page_image, page_num, output_data):
                                     text += " " + str(line_row["text"])
 
                             # replace unicode characters from text to nearly ascii characters
-                            line_dict["text"] = unidecode(text[1:])
+                            line_dict["topleft"] = line_topL
+                            line_dict["text"] = text[1:]
+                            line_dict["bottomright"] = line_bottomR
                             block_dict["textlines"].append(line_dict)
                             flag = True if len(line_dict["text"])>0 else False
 
@@ -78,18 +101,20 @@ def getDataForJson(df_ocr, page_image, page_num, output_data):
                         # if block has no text lines (empty block)
                     else:
                         blk_no -= 1
-                        # output_data["blocks"].append(block_dict)
     return output_data
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
-        "-p",
-        dest="pdf_files_dir",
-        help="enter the path directory containing pdf files",
+        "-i",
+        dest="pdf_file",
+        help="enter the path to pdf file",
         required=True,
+    )
+
+    parser.add_argument(
+        "-s", "--save_images", help="save images of pdf file", action="store_true"
     )
 
     parser.add_argument(
@@ -97,57 +122,71 @@ if __name__ == "__main__":
     )
 
     args = parser.parse_args()
-    print(args)
+    
+    configs = getConfig()
+
+    os.environ['TESSDATA_PREFIX'] = configs['tessdata']  # os.path.join(os.getcwd(), configs['tessdata'])
+
+    file = args.pdf_file
+    print("File Path:", file)
+    pdf_file_name = file.split("/")[-1][:-4]
+    
+    if args.save_images:
+        pdf_img_dir = createDirs(imageDir=configs["pdf_images_dir"], fileName=pdf_file_name)
+
+    if args.debug:
+        debug_img_dir = createDirs(imageDir=configs["debug_images_dir"], fileName=pdf_file_name)
+
+    if not os.path.exists(os.environ['TESSDATA_PREFIX']):
+        os.mkdir(os.environ['TESSDATA_PREFIX'])
 
     # creating blocks directory to store json files
-    block_dir = os.path.join(args.pdf_files_dir, "tesseractJSON")
-    if not os.path.exists(block_dir):
-        os.mkdir(block_dir)
+    if not os.path.exists(configs["output_json_dir"]):
+        os.mkdir(configs["output_json_dir"])   
 
-    pdf_files = glob(os.path.join(args.pdf_files_dir, "*.pdf"))
+    # convert pdf file to images
+    pages = pdf2image.convert_from_path(file)
+    data = {}
+    data["blocks"] = []
 
-    for file in pdf_files:
-        print("File Path:", file)
-        pdf_file_name = file.split("/")[-1][:-4]
+    # download tesseract traineddata file for language if it doesn't exist 
+    if not os.path.isfile(os.path.join(os.environ['TESSDATA_PREFIX'], configs['language'] + ".traineddata")):
+        print(configs['language'] + ".traineddata not found in " + configs['tessdata']  + ". Downloading file:")
+        url = 'https://github.com/tesseract-ocr/tessdata_best/raw/master/' + configs['language'] + '.traineddata'
+        cmd = "sudo wget -O" + os.path.join(os.environ['TESSDATA_PREFIX'], configs['language'] + ".traineddata") + " " + url
+        os.system(cmd)
+        
+    # loop through every page image
+    for page_no, page in enumerate(pages):
+        print("Page No: " + str(page_no + 1) + "/" + str(len(pages)))
+        img = np.array(page)
+        
+        # save pdf images
+        if args.save_images:
+            img_path = os.path.join(
+                pdf_img_dir, pdf_file_name + "_Page" + str(page_no + 1) + ".png"
+            )
+            cv2.imwrite(img_path, img)
 
-        if not os.path.isfile(os.path.join(block_dir, pdf_file_name + ".json")):
-	        # convert pdf file to images
-	        pages = pdf2image.convert_from_path(file)
-	        data = {}
-	        data["blocks"] = []
+        try:
+            # extracting ocra data from image
+            ocr_data = pytesseract.image_to_data(
+                img, lang=configs['language'], output_type=pytesseract.Output.DATAFRAME
+            )
+        except Exception as e:
+            print("OCR Failed:", e)
+            continue
 
-	        # loop through every page image
-	        for page_no, page in enumerate(pages):
-	            print("Page No: " + str(page_no + 1) + "/" + str(len(pages)))
-	            img = np.array(page)
+        # get the data dictionary for json file
+        data = getDataForJson(ocr_data, img, page_no, data)
+            
+        # save output images with bounding boxes
+        if args.debug:
+            img_path = os.path.join(
+                debug_img_dir, pdf_file_name + "_Page" + str(page_no + 1) + ".png"
+            )
+            cv2.imwrite(img_path, img)
 
-	            try:
-	                # extracting ocra data from image
-	                ocr_data = pytesseract.image_to_data(
-	                    img, output_type=pytesseract.Output.DATAFRAME
-	                )
-	            except:
-	                print("OCR Failed.")
-	                continue
-
-	            # get the data dictionary for json file
-	            data = getDataForJson(ocr_data, img, page_no, data)
-
-	            # save output images with bounding boxes
-	            if args.debug:
-	                debug_dir = os.path.join(args.pdf_files_dir, "debug")
-	                # creating debug directory
-	                if not os.path.exists(debug_dir):
-	                    os.mkdir(debug_dir)
-	                # creating directory for each pdf file
-	                imgs_dir = os.path.join(debug_dir, pdf_file_name)
-	                if not os.path.exists(imgs_dir):
-	                    os.mkdir(imgs_dir)
-	                img_path = os.path.join(
-	                    imgs_dir, pdf_file_name + "_Page" + str(page_no + 1) + ".png"
-	                )
-	                cv2.imwrite(img_path, img)
-
-	        # writing python dictionary to json file
-	        with open(os.path.join(block_dir, pdf_file_name + ".json"), "w") as f:
-	            json.dump(data, f)
+    # writing python dictionary to json file
+    with open(os.path.join(configs["output_json_dir"], pdf_file_name + ".json"), "w", encoding="UTF-8") as f:
+        json.dump(data, f, ensure_ascii=False)
